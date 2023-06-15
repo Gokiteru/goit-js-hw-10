209aff591b22565a07893afe30a8349393ab8a9c
Gokiteru <vakin.vlad@gmail.com>
2023-06-16 00:12:51 +0300

    fixed some issues mentioned by mentor & updated parcel

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,7 +1,8 @@
-export default function fetchBreeds() {
-  const BASE_URL = `https://api.thecatapi.com/v1/breeds`;
-  const apiKey =
-    'live_rXLydfuFknLtFmsT7yOyvmJmKPAAJhlYuZOnndTITcIciYTjG3C0CtUEiJT8eQiq';
+const BASE_URL = `https://api.thecatapi.com/v1/breeds`;
+const apiKey =
+  'live_rXLydfuFknLtFmsT7yOyvmJmKPAAJhlYuZOnndTITcIciYTjG3C0CtUEiJT8eQiq';
+
+export function fetchBreeds() {
   return fetch(BASE_URL, {
     headers: {
       'x-api-key': apiKey,
@@ -13,4 +14,20 @@ export default function fetchBreeds() {
 
     return response.json();
   });
-}
+};
+
+export function fetchCatByBreed(breedId) {
+  return fetch(BASE_URL, {
+    headers: {
+      'x-api-key': apiKey,
+      breed_ids: breedId,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      return response.json();
+    })
+  };
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
-import fetchBreeds from './cat-api';
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
 
 const baseUrl = `https://api.thecatapi.com/v1/breeds`;
 const apiKey =
@@ -14,6 +15,7 @@ searchForm.addEventListener('input', onSearch);
 fetchBreeds()
   .then(data => {
     loader.hidden = true;
+    searchForm.hidden = false;
     numberBreeds = data;
 
     for (let i = 0; i < numberBreeds.length; i += 1) {
@@ -36,44 +38,29 @@ fetchBreeds()
 
 function onSearch(evt) {
   const breedId = evt.currentTarget.value;
-  fetchCatByBreed(breedId);
-}
-
-function fetchCatByBreed(breedId) {
   loader.hidden = false;
   clearHTML();
-  fetch(baseUrl, {
-    headers: {
-      'x-api-key': apiKey,
-      breed_ids: breedId,
-    },
+
+  fetchCatByBreed(breedId)
+  .then(data => {
+    loader.hidden = true;
+    numberBreeds = data;
+    if (!numberBreeds[breedId].image) {
+      Notiflix.Notify.warning(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+      throw new Error(data.statusText);
+    }
+    const catCard = `<img src="${numberBreeds[breedId].image.url}" width="500 px" alt="cat image"><h2>${numberBreeds[breedId].name}</h2><p>${numberBreeds[breedId].description}</p>`;
+
+    showInfo.innerHTML = catCard;
+    showInfo.style.fontSize = '20px';
   })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-
-      return response.json();
-    })
-    .then(data => {
-      loader.hidden = true;
-      numberBreeds = data;
-      if (!numberBreeds[breedId].image) {
-        Notiflix.Notify.warning(
-          'Oops! Something went wrong! Try reloading the page!'
-        );
-        throw new Error(data.statusText);
-      }
-      const catCard = `<img src="${numberBreeds[breedId].image.url}" width="500 px" alt="cat image"><h2>${numberBreeds[breedId].name}</h2><p>${numberBreeds[breedId].description}</p>`;
-
-      showInfo.innerHTML = catCard;
-      showInfo.style.fontSize = '20px';
-    })
-    .catch(function (error) {
-      console.log(error);
-      clearHTML();
-      return;
-    });
+  .catch(function (error) {
+    console.log(error);
+    clearHTML();
+    return;
+  });
 }
 
 function clearHTML() {
